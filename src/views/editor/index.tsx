219de461cf74be690f2d76b41daf64ff588168b0
swlws <979623074@qq.com>
2024-05-12 18:17:08 +0800

    layout

--- src/views/editor/index.tsx
+++ src/views/editor/index.tsx
@@ -1,3 +1,22 @@
+// 组件
+import Material from "./components/material";
+import DrawBoard from "./components/draw-board";
+import Attribute from "./components/attribute";
+
+import "./index.scss";
+
 export default function () {
-  return <>editor</>;
+  return (
+    <article className="vc-layout">
+      <header className="vc-layout--left">
+        <Material />
+      </header>
+      <main className="vc-layout--middle">
+        <DrawBoard />
+      </main>
+      <footer className="vc-layout--right">
+        <Attribute />
+      </footer>
+    </article>
+  );
 }
