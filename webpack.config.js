693d70c1ccbc6235c5ab5eb60a22e5452c0654bc
swlws <979623074@qq.com>
2024-05-12 16:06:20 +0800

    支持ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,45 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 // const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js',
+  entry: "./src/index.tsx",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: "./dist",
     },
-    devServer: {
-        static: {
-            directory: './dist',
-        },
-        port: 9000,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
-                    },
-                },
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-    plugins: [
-        // new ModuleFederationPlugin({
-        //     name: 'mainApp',
-        //     remotes: {
-        //         microApp1: 'microApp1@http://localhost:3002/remoteEntry.js',
-        //         // microApp2: 'microApp2@http://localhost:3003/remoteEntry.js',
-        //     },
-        // }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-        }),
+    port: 9000,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        enforce: "pre",
+        exclude: /node_modules/,
+        use: ["babel-loader", "ts-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
+  },
+  plugins: [
+    // new ModuleFederationPlugin({
+    //     name: 'mainApp',
+    //     remotes: {
+    //         microApp1: 'microApp1@http://localhost:3002/remoteEntry.js',
+    //         // microApp2: 'microApp2@http://localhost:3003/remoteEntry.js',
+    //     },
+    // }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
